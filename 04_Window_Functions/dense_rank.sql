9032239adb2f550df2577023aa6727f445d108fa
Perseverance Mutengera <123206539+Persemutengera@users.noreply.github.com>
2026-09-20 15:42:55 +0200

    Add SQL query for comparing ranking functions
    Added SQL query to compare ROW_NUMBER, RANK, and DENSE_RANK functions.

--- 04_Window_Functions/dense_rank.sql
+++ 04_Window_Functions/dense_rank.sql
@@ -1,1 +1,9 @@
-
+-- let's compare this to dense rank
+SELECT dem.employee_id, dem.first_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary desc) row_num,
+Rank() OVER(PARTITION BY gender ORDER BY salary desc) rank_1,
+dense_rank() OVER(PARTITION BY gender ORDER BY salary desc) dense_rank_2 -- this is numerically ordered instead of positional like rank
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+;
